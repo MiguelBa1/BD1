5054b0747e06a81749879053b349b213cbe94e6e
Miguel Barrera <59462086+MiguelBa1@users.noreply.github.com>
2022-06-26 15:01:16 -0500

    correcion

--- configuraciones/conexion.php
+++ configuraciones/conexion.php
@@ -2,6 +2,6 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$DB = "tienda";
+$DB = "clase";
 $conn = new mysqli($host, $user, $pass, $DB) or die("Error al conectar a la DB " . mysqli_error($link));
 ?>
