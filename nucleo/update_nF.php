f57e4377ba8cc77add5b4f5e732f4c097555937c
AlejoAb <aalvarezbo@unal.edu.co>
2022-06-27 12:49:10 -0500

    Mejoras generales

--- nucleo/update_nF.php
+++ nucleo/update_nF.php
@@ -4,15 +4,25 @@
 require('../configuraciones/conexion.php');
 
 
+if($_POST["casados"] === "Si" and ($_POST["fecha_de_matrimonio"] === "" or $_POST["acta_de_matrimonio"] === "")){
+	echo "Por favor ingrese una fecha de matrimonio y un acta de matrimonio";
+}else{
+if($_POST["casados"] === "No" or $_POST["casados"] == ""){
+	$query="UPDATE nucleo_familiar SET  numero_celular = '$_POST[numero_celular]', dinero ='$_POST[dinero]' WHERE codigo = '$_POST[codigo]'";
+}
+else{
+	$query="UPDATE nucleo_familiar SET  numero_celular = '$_POST[numero_celular]', dinero ='$_POST[dinero]', fecha_de_matrimonio ='$_POST[fecha_de_matrimonio]', acta_de_matrimonio ='$_POST[acta_de_matrimonio]' WHERE codigo = '$_POST[codigo]'";
+}
 
 
-$query="UPDATE nucleo_familiar SET  numero_celular = '$_POST[numero_celular]', dinero ='$_POST[dinero]' WHERE codigo = '$_POST[codigo]'";
-
-$result = mysqli_query($conn, $query) or die(mysqli_error($conn));
-
-if($result) {
-	header ("Location: nucleo.php");
-} else {
+try {
+	$result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+	if ($result) {
+		header("Location: nucleo.php");
+	} 
+} catch (\Throwable $th) {
 	echo "Ha ocurrido un error al actualizar el nucleo familiar";
+	echo $th;
+}
 }
 ?>
